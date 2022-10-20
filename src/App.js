11bbd3ca1232640884d9934835918ccd4100d7ec
asibeyd <asibey.d@northeastern.edu>
2022-10-19 23:33:06 -0400

    "a6-changes"

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import Tuiter from "./tuiter";
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
 
-
-
-
 function App() {
     return (
         <BrowserRouter>
         <div className="container">
             <Routes>
+                <Route path="/labs"
+                       element={<Labs/>}/>
                 <Route path="/hello"
                        element={<HelloWorld/>}/>
+                <Route path="/tuiter"
+                       element={<Tuiter/>}/>
             </Routes>
-            <HelloWorld/>
-            <Labs/>
-            <Tuiter/>
+
         </div>
         </BrowserRouter>
 
